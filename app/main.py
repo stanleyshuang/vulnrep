8ec9062d4245972e32bcff41dfcf26e6d4f9f37e
stanleyshuang <stanleyshuang@qnap.com>
2021-06-05 11:27:11 +0800

    parse the link to Salesforce and correct the format if needed

--- app/main.py
+++ app/main.py
@@ -11,6 +11,23 @@ import sys
 from jira import JIRA
 from util.util_text_file import get_lines, flush_text
 
+def extract_str_in_link(content):
+    import re
+    # regex to extract required strings
+    reg_str = r"\[(.*?)\]"
+    in_bracket = re.search(reg_str, content)
+    if in_bracket:
+        res = in_bracket.group(1).split('|')
+        if not res or len(res)<=2:
+            return '', '', content
+        return res[0], res[len(res)-1], content[in_bracket.end():]
+    return '', '', content
+
+def parse_salesforce_link(content):
+    name, link, others = extract_str_in_link(content)
+    return name, link, others
+    
+
 ### get argv[1] as input
 if len(sys.argv) >=4:
     username = sys.argv[1]
@@ -27,6 +44,10 @@ jira = JIRA(basic_auth=(username, password), options={'server': server})
 issue = jira.issue(jira_id)
 summary = issue.fields.summary         # 'Field level security permissions'
 votes = issue.fields.votes.votes       # 440 (at least)
+description = issue.fields.description
 print(summary)
 print(votes)
-
+name, link, others = parse_salesforce_link(description)
+if len(name) > 0 and len(link) > 0:
+    print('--- Correct Salesforce link [{name}|{link}]'.format(name=name, link=link))
+    issue.update(description = '[{name}|{link}]{others}'.format(name=name, link=link, others=others))
